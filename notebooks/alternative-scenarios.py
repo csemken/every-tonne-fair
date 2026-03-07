31bd89beeac07592d8058911df76c8312ffc5c36
csemken <csemken@users.noreply.github.com>
2026-03-07 17:16:55 -0500

    Correlate rho, T at the projection level

--- notebooks/alternative-scenarios.py
+++ notebooks/alternative-scenarios.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: .conda
 #     language: python
 #     name: python3
 # ---
--- notebooks/correlate_rho_T.py
+++ notebooks/correlate_rho_T.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: .conda
 #     language: python
 #     name: python3
 # ---
@@ -25,10 +25,24 @@ irf = xr.load_dataset('../output/irf_1GtCO2_new_scenarios.nc')
 
 # %%
 # pooling 10 new scenarios
-irf_2100 = irf['temperature'].sel(timebounds=2101).mean(dim='scenario')
+irf_2100 = (
+    irf["temperature"]
+    .sel(
+        scenario=[
+            "irf_2021-11-09_1__high__C__SSP1BL__exclude",
+            "irf_2021-11-09_1__low__U__SSP1BL__exclude",
+        ],
+        timebounds=2101,
+    )
+    .to_dataframe()
+    .reset_index()
+    .assign(scenario=lambda df: df["scenario"].str.removeprefix("irf_"))
+    .rename(columns={"temperature": "irf"})
+)
+irf_2100
 
 # %%
-plt.hist(irf_2100)
+plt.hist(irf_2100["irf"])
 
 # %%
 proj = xr.load_dataset('../output/scenario_projections_fair.nc')
@@ -38,26 +52,39 @@ proj = xr.load_dataset('../output/scenario_projections_fair.nc')
 proj.scenario
 
 # %%
-proj_2100 = proj['temperature'].sel(scenario=['2021-11-09_1__high__C__SSP1BL__exclude', '2021-11-09_1__low__U__SSP1BL__exclude'], timebounds=2101).mean(dim='scenario')
+proj_2100 = (
+    proj["temperature"]
+    .sel(
+        scenario=[
+            "2021-11-09_1__high__C__SSP1BL__exclude",
+            "2021-11-09_1__low__U__SSP1BL__exclude",
+        ],
+        timebounds=2101,
+    )
+    .to_dataframe()
+    .reset_index()
+)
 
 # %%
-plt.hist(proj_2100)
+plt.hist(proj_2100["temperature"])
 
 # %%
-plt.scatter(irf_2100, proj_2100)
+df = irf_2100.merge(proj_2100, on=["scenario", "config", "timebounds"])
+plt.scatter(df["irf"], df["temperature"])
 plt.xlabel('IRF 2100 K/GtCO2')
 plt.ylabel('Temperature anomaly 2100 Current policies K')
 
 # %%
-df_out = pd.DataFrame(np.array((irf_2100.data, proj_2100.data)).T, columns=['irf', 'warming'], index=irf_2100.config)
+df_out = df[["scenario", "config", "irf", "temperature"]].set_index(["scenario", "config"])
+df_out
 
 # %%
 df_out.to_csv('../output/irf_warming.csv')
 
 # %%
-statsmodels.graphics.gofplots.qqplot_2samples(irf_2100.data, proj_2100.data, xlabel=None, ylabel=None, line="r", ax=None)
+statsmodels.graphics.gofplots.qqplot_2samples(df["irf"], df["temperature"], xlabel=None, ylabel=None, line="r", ax=None)
 
 # %%
-scipy.stats.linregress(irf_2100.data, proj_2100.data)
+scipy.stats.linregress(df["irf"], df["temperature"])
 
 # %%
